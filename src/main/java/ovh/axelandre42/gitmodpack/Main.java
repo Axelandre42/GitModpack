d9fd7abd5ece9aa970ca253bce8bd3e70df3dd9f
Alexandre Waeles <5228298+Axelandre42@users.noreply.github.com>
2019-11-16 21:44:02 +0100

    Starting again from scratch

--- src/main/java/ovh/axelandre42/gitmodpack/Main.java
+++ src/main/java/ovh/axelandre42/gitmodpack/Main.java
@@ -1,16 +1,11 @@
 package ovh.axelandre42.gitmodpack;
 
-import java.io.File;
 import java.net.MalformedURLException;
-import java.net.URL;
 
 public class Main {
-    
+
     public static void main(String[] args) throws MalformedURLException {
-        Updater updater = new Updater(new File(args[0]), new URL(args[1]), new File(args[0] + File.separator + "gitmeta.json"));
-        if (updater.check())
-            updater.update();
-        
+
     }
 
 }
